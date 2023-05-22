e7626329749cc0dbe3016c1ec879affec57b2f9b
Shashank Deshpande <128305551+ShashankD7@users.noreply.github.com>
2023-05-22 15:45:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-function threeSum(arr, target) {
-// write your code here
-  
-}
+function threeSum(nums, target) {
+  // Sort the array in ascending order
+  nums.sort((a, b) => a - b);
+
+  let closestSum = Infinity;
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+
+      if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+        closestSum = sum;
+      }
+
+      if (sum < target) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
 
 module.exports = threeSum;
